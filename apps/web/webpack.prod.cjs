e33c51c4551bc87b3fe4a41a78cf92b87f26e26c
Byonghun Kwak <byonghunkwak@gmail.com>
2025-09-25 15:37:39 -0700

    Installing missing html webpack plugin dependency

--- apps/web/webpack.prod.cjs
+++ apps/web/webpack.prod.cjs
@@ -2,6 +2,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "production",
